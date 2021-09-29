fead8152e878229f78a6991dc8d1e21ba5eaf30f
Andy Holtzmann <aholtzmann@equinix.com>
2021-09-29 17:28:01 -0600

    cuddled assignments

--- cmd/exec.go
+++ cmd/exec.go
@@ -43,6 +43,7 @@ func init() {
 func doExec(cmd *cobra.Command, args []string) {
 	ctx, shutdown := initTracer()
 	defer shutdown()
+
 	ctx = loadTraceparent(ctx, traceparentCarrierFile)
 	tracer := otel.Tracer("otel-cli/exec")
 
