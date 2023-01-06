efb56089621b016af4d5d2ded448f24879882e6a
Amy Tobey <tobert@gmail.com>
2023-01-06 13:27:11 -0500

    #42 add version subcommand (#114)
    * switch some tests to cmp.Diff and fix them
    
    While testing the new version functionality (not committed yet), my
    tests failed on these. They seem to have been broken a while so I also
    need to look into why previous test runs haven't failed me.
    
    Switched to cmp.Diff because it shows exactly what happened with little
    code where t.Fail() is a decision past-me made that I'm annoyed by now.
    
    * add version formatting function & test
    
    Adds a function to format version from ldflags. I originally open coded
    this in main() and as the feature came together it made more sense in
    the otelcli package so I moved it & tests here.
    
    * add support for otel-cli --version
    
    Adds vars in main that can be set by ldflags, and wires them up into the
    Cobra startup code in otelcli/root.go. At this point it should "just
    work" with goreleaser.

---
+++ otelcli/version.go
@@ -0,0 +1,27 @@
+package otelcli
+
+import "strings"
+
+// FormatVersion pretty-prints the global version, commit, and date values into
+// a string to enable the --version flag. Public to be called from main.
+func FormatVersion(version, commit, date string) (string) {
+	parts := []string{}
+
+	if version != "" {
+		parts = append(parts, version)
+	}
+
+	if commit != "" {
+		parts = append(parts, commit)
+	}
+
+	if date != "" {
+		parts = append(parts, date)
+	}
+
+	if len(parts) == 0 {
+		parts = append(parts, "unknown")
+	}
+
+	return strings.Join(parts, " ")
+}
